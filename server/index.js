9160cfca0df3c89a2f90d1263ede5e753c812a4f
Irfan <bhattiirfaniqbal@gmail.com>
2022-06-30 19:21:20 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
-  .catch(()=>console.log("data base conected"))
+  .then(()=>console.log("data base conected"))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
